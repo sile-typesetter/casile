9182b3b80c2bdc9034c9ae65ef2a9549ee8a252f
Caleb Maclennan <caleb@alerque.com>
2015-11-30 17:14:16 +0200

    Tweak margins by pulling the header in 2mm

--- viachristus.lua
+++ viachristus.lua
@@ -64,7 +64,7 @@ local book = SILE.require("classes/book");
 book:loadPackage("masters")
 book:defineMaster({ id = "right", firstContentFrame = "content", frames = {
   content = {left = "22.5mm", right = "100%-15mm", top = "20mm", bottom = "top(footnotes)" },
-  runningHead = {left = "left(content)", right = "right(content)", top = "top(content)-10mm", bottom = "top(content)-2mm" },
+  runningHead = {left = "left(content)", right = "right(content)", top = "top(content)-8mm", bottom = "top(content)-2mm" },
   footnotes = { left="left(content)", right = "right(content)", height = "0", bottom="100%-15mm"}
 }})
 book:defineMaster({ id = "left", firstContentFrame = "content", frames = {}})
