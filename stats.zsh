ddb23701396f53fbb4e2c28a40b87522a12a797e
Caleb Maclennan <caleb@alerque.com>
2015-12-04 13:38:32 +0200

    Fix cross author and date range rename tracking

--- stats.zsh
+++ stats.zsh
@@ -21,29 +21,31 @@ git log --format=%aN --follow -- "$file" |
         for i in $(seq 0 6); do
             let month=0
             since=$(date --date "$cyclestart - $i months" "+%F")
-            until=$(date --date "$since + 1 month" "+%F")
-            [[ $(date --date "$until" "+%s") -le $rootstart ]] && break
+            until=$(date --date "$since + 1 month" "+%s")
+            [[ $until -le $rootstart ]] && break
             start=$(date --date "$since" "+%Y-%m")
             echo
             echo "Month $start"
             echo "-------------"
-            git log --format='%h %s' --author="$author" \
-                --since="$since" --until="$until" \
+            git log --format='%at|%h|%s|%an' \
+                --since="$since" \
                 --follow --find-renames -- "$1" |
-				tee /dev/stderr |
-                while read sha1 msg; do
+                while IFS='|' read ts sha1 msg aut; do
                     after=$(git show "$sha1":"$file" 2>&- | countchars)
 					git -c core.quotepath=off log -1 $sha1 --stat --find-renames |
 						grep ' => ' |
 						perl -pne 's/ (.*) => (.*) \| .*/\1|\2/g;s/"//g' |
 						IFS="|" read oldname newname
 					[[ $newname == $file ]] && file="$oldname"
+					[[ $ts -le $until ]] || continue
+					[[ $aut == $author ]] || continue
                     before=$(git show "$sha1"^:"$file" 2>&- | countchars)
                     change=$(($after-$before))
                     [[ $change -le 0 ]] && continue
                     let month=$month+$change
                     echo "$sha1  $(printf %10d $change) $msg"
                 done
+				echo
                 echo "Total:   $(printf %10d $month)"
             let user=$user+$month
         done
