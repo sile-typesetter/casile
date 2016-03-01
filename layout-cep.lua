5722f71d8ca26715e979ed03cdc08a52e2fa863b
Caleb Maclennan <caleb@alerque.com>
2016-03-01 20:42:12 +0200

    Use narrower margins and no footer frame for pocket size

--- layout-cep.lua
+++ layout-cep.lua
@@ -1,9 +1,7 @@
 local book = SILE.require("classes/book");
-book:loadPackage("masters")
 book:defineMaster({ id = "right", firstContentFrame = "content", frames = {
-  content = {left = "22.5mm", right = "100%-15mm", top = "20mm", bottom = "top(footnotes)" },
-  runningHead = {left = "left(content)", right = "right(content)", top = "top(content)-8mm", bottom = "top(content)-2mm" },
-  footnotes = { left="left(content)", right = "right(content)", height = "0", bottom="100%-15mm"}
+  content = {left = "20mm", right = "100%-12.5mm", top = "20mm", bottom = "100%-15mm" },
+  runningHead = {left = "left(content)", right = "right(content)", top = "top(content)-10mm", bottom = "top(content)-2mm" },
 }})
 book:defineMaster({ id = "left", firstContentFrame = "content", frames = {}})
 book:loadPackage("twoside", { oddPageMaster = "right", evenPageMaster = "left" });
