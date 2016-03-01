626f00de5c51367d2edb48b3a7c06f4ae4b678be
Caleb Maclennan <caleb@alerque.com>
2016-03-01 20:41:52 +0200

    Use bigger margins and single side template for A4 drafts

--- layout-a4.lua
+++ layout-a4.lua
@@ -1,9 +1,9 @@
 local book = SILE.require("classes/book");
 book:loadPackage("masters")
 book:defineMaster({ id = "right", firstContentFrame = "content", frames = {
-  content = {left = "22.5mm", right = "100%-15mm", top = "20mm", bottom = "top(footnotes)" },
-  runningHead = {left = "left(content)", right = "right(content)", top = "top(content)-8mm", bottom = "top(content)-2mm" },
-  footnotes = { left="left(content)", right = "right(content)", height = "0", bottom="100%-15mm"}
+  content = {left = "32mm", right = "100%-32mm", top = "36mm", bottom = "top(footnotes)" },
+  runningHead = {left = "left(content)", right = "right(content)", top = "top(content)-12mm", bottom = "top(content)-2mm" },
+  footnotes = { left="left(content)", right = "right(content)", height = "0", bottom="100%-24mm"}
 }})
 book:defineMaster({ id = "left", firstContentFrame = "content", frames = {}})
 book:loadPackage("twoside", { oddPageMaster = "right", evenPageMaster = "left" });
