e6e9615a83ebd55b8e67c94c6711e00b51d48bbd
Caleb Maclennan <caleb@alerque.com>
2016-03-04 17:50:36 +0200

    Add utility to markup diffs with CriticMark for Marked2

---
+++ bin/diff2marked.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cat - > /dev/null
+cd $MARKED_ORIGIN
+git diff --no-color --word-diff -U99999 -- $MARKED_PATH |
+	sed -e '1,5d;s/\[-/{--/g;s/-\]/--}/g;s/{\+/{++/g;s/\+}/++}/g'
