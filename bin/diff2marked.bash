c6af73103ef0f77a0c86ce4058c6e44c6a724dc4
Caleb Maclennan <caleb@alerque.com>
2016-03-04 18:55:43 +0200

    Handle case of no differences in diff preprocessor

--- bin/diff2marked.bash
+++ bin/diff2marked.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-cat - > /dev/null
 cd $MARKED_ORIGIN
-git diff --no-color --word-diff -U99999 -- $MARKED_PATH |
-	sed -e '1,5d;s/ *{[-\.].*}$//g;s/\[-/{--/g;s/-\]/--}/g;s/{\+/{++/g;s/\+}/++}/g'
+if git diff --quiet -- $MARKED_PATH; then
+    cat -
+    exit
+else
+    cat - > /dev/null
+    git diff --no-color --word-diff -U99999 -- $MARKED_PATH |
+        sed -e '1,5d;s/ *{[-\.].*}$//g;s/\[-/{--/g;s/-\]/--}/g;s/{\+/{++/g;s/\+}/++}/g'
+fi
