5d95ed595d97773b1120341e3f90c5dbc0359508
Caleb Maclennan <caleb@alerque.com>
2016-03-04 18:37:44 +0200

    Filter out Pandoc class tags from Marked2 preview

--- bin/diff2marked.bash
+++ bin/diff2marked.bash
@@ -3,4 +3,4 @@
 cat - > /dev/null
 cd $MARKED_ORIGIN
 git diff --no-color --word-diff -U99999 -- $MARKED_PATH |
-	sed -e '1,5d;s/\[-/{--/g;s/-\]/--}/g;s/{\+/{++/g;s/\+}/++}/g'
+	sed -e '1,5d;s/ *{[-\.].*}$//g;s/\[-/{--/g;s/-\]/--}/g;s/{\+/{++/g;s/\+}/++}/g'
