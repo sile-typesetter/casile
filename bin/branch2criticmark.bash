83a5366a8f10927a3407166c053f69c3046e379e
Caleb Maclennan <caleb@alerque.com>
2016-04-04 20:35:35 +0300

    Ignore add/del when highlights are involved

--- bin/branch2criticmark.bash
+++ bin/branch2criticmark.bash
@@ -3,7 +3,10 @@ if git diff ${1}..HEAD --quiet -- ${2}; then
     cat $1 
 else
     git diff --no-color --word-diff -U99999 ${1}..HEAD -- ${2} |
-        sed -e '1,5d' \
+    sed -e '1,5d' |
+        perl -pn \
+            -e 's/\[-.*?-\]\{\+(.*?==.*?)\+\}/\1/g' |
+        sed \
             -e 's/\[-/{--/g' -e 's/-\]/--}/g' \
             -e 's/{+/{++/g' -e 's/+}/++}/g'
 fi
