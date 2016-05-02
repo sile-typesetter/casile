894813f4403da22d1001e2497538c7de31d42a39
Caleb Maclennan <caleb@alerque.com>
2016-05-02 23:35:17 +0300

    Refactor branch to criticmark to preprocess with M4

--- bin/branch2criticmark.bash
+++ bin/branch2criticmark.bash
@@ -1,12 +1,23 @@
-#!/usr/bin/env bash
-if git diff ${1}..HEAD --quiet -- ${2}; then
-    cat $1 
+#!/usr/bin/env zsh
+WT=$(mktemp -d -u worktree-diff.XXXXXX)
+
+trap 'rm -rf ${WT}' EXIT SIGHUP SIGTERM
+
+macros=avadanlik/viachristus.m4
+
+git worktree add ${WT} ${1}
+
+m4 ${macros} ${WT}/${2} | git hash-object --stdin -w | read A
+m4 ${macros} ${2} | git hash-object --stdin -w | read B
+
+if git diff ${A}..${B} --quiet; then
+    git show ${B}
 else
-    git diff --no-color --word-diff -U99999 ${1}..HEAD -- ${2} |
-    sed -e '1,5d' |
-        perl -pn \
-            -e 's/\[-[^\]]*?-\]\{\+([^\}]*?==.*?)\+\}/\1/g' |
-        sed \
-            -e 's/\[-/{--/g' -e 's/-\]/--}/g' \
-            -e 's/{+/{++/g' -e 's/+}/++}/g'
+    git diff --no-color --word-diff -U99999 ${A}..${B} |
+        sed -e '1,5d' |
+            perl -pn \
+                -e 's/\[-[^\]]*?-\]\{\+([^\}]*?==.*?)\+\}/\1/g' |
+            sed \
+                -e 's/\[-/{--/g' -e 's/-\]/--}/g' \
+                -e 's/{+/{++/g' -e 's/+}/++}/g'
 fi
