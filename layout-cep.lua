f0ec608520d99b0cfec62a5375ed4bd667d81c0f
Caleb Maclennan <caleb@alerque.com>
2016-07-28 17:17:58 +0300

    Fix name of unused but commonly referenced frame in pocket layout

--- layout-cep.lua
+++ layout-cep.lua
@@ -2,7 +2,7 @@ local book = SILE.require("classes/book");
 book:defineMaster({ id = "right", firstContentFrame = "content", frames = {
   content = {left = "20mm", right = "100%pw-12.5mm", top = "20mm", bottom = "100%ph-15mm" },
   runningHead = {left = "left(content)", right = "right(content)", top = "top(content)-10mm", bottom = "top(content)-2mm" },
-  endnotes = {left = "20mm", right = "100%pw-12.5mm", top = "0", height = "10mm" },
+  footnotes = {left = "20mm", right = "100%pw-12.5mm", top = "0", height = "0" },
 }})
 book:defineMaster({ id = "left", firstContentFrame = "content", frames = {}})
 book:loadPackage("twoside", { oddPageMaster = "right", evenPageMaster = "left" });
