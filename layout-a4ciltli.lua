6e756c52d80b306dc5c877cdba3b95103bc96396
Caleb Maclennan <caleb@alerque.com>
2016-11-15 11:51:18 +0300

    Bring bound A4 layout up to speed with frames

--- layout-a4ciltli.lua
+++ layout-a4ciltli.lua
@@ -1,14 +1,37 @@
-local book = SILE.require("classes/book");
-book:loadPackage("masters")
-book:defineMaster({ id = "right", firstContentFrame = "content", frames = {
-  content = { left = "32mm", right = "100%pw-32mm", top = "36mm", bottom = "top(footnotes)" },
-  runningHead = { left = "left(content)", right = "right(content)", top = "top(content)-12mm", bottom = "top(content)-2mm" },
-  footnotes = { left = "left(content)", right = "right(content)", height = "0", bottom = "100%ph-24mm"}
-}})
-book:defineMaster({ id = "left", firstContentFrame = "content", frames = {} })
-book:loadPackage("twoside", { oddPageMaster = "right", evenPageMaster = "left" });
-book:mirrorMaster("right", "left")
-SILE.call("switch-master-one-page", {id = "right"})
+local class = SILE.documentState.documentClass
+
+class:defineMaster({
+    id = "right",
+    firstContentFrame = "content",
+    frames = {
+      content = {
+        left = "38mm",
+        right = "100%pw-26mm",
+        top = "bottom(runningHead)+4mm",
+        bottom = "top(footnotes)-4mm"
+      },
+      runningHead = {
+        left = "left(content)",
+        right = "right(content)",
+        top = "24mm",
+        bottom = "30mm"
+      },
+      footnotes = {
+        left = "left(content)",
+        right = "right(content)",
+        height = "0",
+        bottom = "top(folio)-4mm"
+      },
+      folio = {
+        left = "left(content)",
+        right = "right(content)",
+        top = "100%ph-30mm",
+        bottom = "100%ph-24mm"
+      }
+    }
+  })
+class:mirrorMaster("right", "left")
+SILE.call("switch-master-one-page", { id = "right" })
 
 SILE.registerCommand("meta:distribution", function (options, content)
   SILE.call("font", { weight = 600, style = "Bold" }, { "Yayın: " })
