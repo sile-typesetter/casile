182538ae888ff4b556d6f1be9211dd415cb6c096
Caleb Maclennan <caleb@alerque.com>
2016-11-23 18:01:15 +0300

    Show some context where names were found

--- bin/sort_names.zsh
+++ bin/sort_names.zsh
@@ -2,12 +2,14 @@
 
 while read name; do
 	clear
+	rg --color=always -m 5 "$name" < *.md | perl -pne "s/.*?(.{0,50}$name.{0,20}).*/\\1/g"
 	echo "\n\n\t$name\n\n"
-	read -k "reply?Is this a full proper name in English, Turkish, Other, None or Skip? (e/t/o/n/s)" < /dev/tty
+	read -k "reply?Is this a full proper name in English, Turkish, Other, None, Partial, or Skip? (e/t/o/n/p/s)" < /dev/tty
 	case $reply in
 		e|E) echo $name >> avadanlik/names.en.txt ;;
 		t|T) echo $name >> avadanlik/names.tr.txt ;;
 		o|O) echo $name >> avadanlik/names.und.txt ;;
+		p|P) echo $name >> avadanlik/names.part.txt ;;
 		n|n) echo $name >> avadanlik/names.xx.txt ;;
 	esac
 done
