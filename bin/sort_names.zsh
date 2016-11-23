eb39ed3e51fe1ebe17e35411fdb854fa991b1945
Caleb Maclennan <caleb@alerque.com>
2016-11-23 17:41:15 +0300

    Also keep track of things that look like names but aren't

--- bin/sort_names.zsh
+++ bin/sort_names.zsh
@@ -3,10 +3,11 @@
 while read name; do
 	clear
 	echo "\n\n\t$name\n\n"
-	read -k "reply?Is this a full proper name in English, Turkish, or any other language? (e/t/o/n)" < /dev/tty
+	read -k "reply?Is this a full proper name in English, Turkish, Other, None or Skip? (e/t/o/n/s)" < /dev/tty
 	case $reply in
 		e|E) echo $name >> avadanlik/names.en.txt ;;
 		t|T) echo $name >> avadanlik/names.tr.txt ;;
-		o|O|u|U) echo $name >> avadanlik/names.und.txt ;;
+		o|O) echo $name >> avadanlik/names.und.txt ;;
+		n|n) echo $name >> avadanlik/names.xx.txt ;;
 	esac
 done
