5611c9c547799a587cabd76a30654ec23909984c
Caleb Maclennan <caleb@alerque.com>
2016-11-24 22:04:24 +0300

    Sketch out more of what the reference extractor needs to do

--- bin/extract_references.js
+++ bin/extract_references.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
 var readline = require('readline');
-var bcv_parser = require('bible-passage-reference-parser/js/full_bcv_parser').bcv_parser;
+var bcv_parser = require('bible-passage-reference-parser/js/tr_bcv_parser').bcv_parser;
 var bcv = new bcv_parser();
 
-function extract_references (data) {
-  console.log("DATA:", data);
-  var res = bcv.parse(data);
-  console.log("RESULTS:", res.osis());
-}
+bcv.set_options({
+  'sequence_combination_strategy': 'separate'
+});
+bcv.include_apocrypha(true);
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -16,4 +15,20 @@ var rl = readline.createInterface({
   terminal: false
 });
 
+var references = [];
+
+function extract_references (data) {
+  // console.log("DATA:", data);
+  var res = bcv.parse(data);
+  references.push(res.osis());
+  // rl.write(res.osis());
+  // console.log("RESULTS:", res.osis());
+}
+
+function output_references () {
+  console.log(references);
+}
+
 rl.on('line', extract_references);
+
+rl.on('close', output_references);
