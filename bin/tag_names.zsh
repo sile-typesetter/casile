b288cb36a583647d32efb288a56dffa0e1f188a6
Caleb Maclennan <caleb@alerque.com>
2016-11-24 15:08:38 +0300

    Don't act on footnote or header lines and condense review output

--- bin/tag_names.zsh
+++ bin/tag_names.zsh
@@ -8,9 +8,10 @@ for source in $@; do
 		clear
 		git co -- $source
 		msg="Tag instances of name '$name' as language '$lang'"
-		perl -i -pne "s/(?<!\})$name(?!\})/\\\\lang$lang{$name}/g" -- $source
+		perl -i -pne "s/(?<!\})$name(?!\})/\\\\lang$lang{$name}/g if ! /^(\[\^\d+\]|#+ )/" -- $source
 		git add -- $source
-		git --no-pager diff --cached --word-diff=color --ignore-all-space -- $source
+		git --no-pager diff --cached -U0 --word-diff=color --minimal --ignore-all-space -- $source |
+			grep -v '@@'
 		git diff-index --quiet --cached HEAD && continue 1
 		read -q "?$msg? (y/n)" || continue 1
 		git commit -m "[auto] $msg"
