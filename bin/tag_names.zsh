77f1746d1c56cac2e19df6de8a6e6d4a269b7ac8
Caleb Maclennan <caleb@alerque.com>
2016-11-24 13:27:20 +0300

    Only find names that aren't already wrapped in language tags

--- bin/tag_names.zsh
+++ bin/tag_names.zsh
@@ -5,12 +5,14 @@ names=$1 ; shift
 
 for source in $@; do
 	while read name; do
+		clear
 		git co -- $source
 		msg="Tag instances of name '$name' as language '$lang'"
-		perl -i -pne "s/$name/\\\\lang$lang{$name}/g" $source
-		git diff --word-diff=color --ignore-all-space -- $source
-		read -q "?$msg? (y/n)" || continue 1
+		perl -i -pne "s/(?<!\})$name(?!\})/\\\\lang$lang{$name}/g" -- $source
 		git add -- $source
+		git --no-pager diff --cached --word-diff=color --ignore-all-space -- $source
+		git diff-index --quiet --cached HEAD && continue 1
+		read -q "?$msg? (y/n)" || continue 1
 		git commit -m "[auto] $msg"
 	done < $names
 done
