9e0e3c487ca945df402dc2f0804cede55999a32b
Caleb Maclennan <caleb@alerque.com>
2016-11-24 17:09:20 +0300

    Split names into 3+ letter segments and tag by block

--- bin/tag_names.zsh
+++ bin/tag_names.zsh
@@ -18,8 +18,18 @@ function tag_name () {
 	git commit -m "[auto] $msg"
 }
 
+# First catch instances of whole names
 for file in $@; do
 	perl -e 'print sort { length($b) <=> length($a) } <>' < $names |
+		while read name; do
+			tag_name $file $name
+		done
+done
+
+# Next try permutations of first, last, etc. (within reason)
+for file in $@; do
+	perl -ne 's/ /\n/g;print if /^.{3,}/' < $names |
+		perl -e 'print sort { length($b) <=> length($a) } <>' |
 		while read name; do
 			tag_name $file $name
 		done
