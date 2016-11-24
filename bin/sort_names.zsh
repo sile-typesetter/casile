5a719aaa6eb67bf6bc92fe9d6044cc9dab019a7c
Caleb Maclennan <caleb@alerque.com>
2016-11-24 11:16:41 +0300

    Show more context up to screen width

--- bin/sort_names.zsh
+++ bin/sort_names.zsh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 
+HALF=$(($COLUMNS/2-20))
+
 while read name; do
 	clear
-	rg --color=always "$name" < *.md | perl -pne "s/.*?(.{0,50}$name.{0,20}).*/\\1/g" | cut -b1-$COLUMNS
+	rg --color=always "$name" < *.md | perl -pne "s/.*?(.{0,$HALF}$name.*)/\\1/g" | cut -b1-$COLUMNS
 	echo "\n\n\t$name\n\n"
 	read -k "reply?Is this a full proper name in English, Turkish, Other, None, Partial, Skip, or Quit? (e/t/o/n/p/s/q)" < /dev/tty
 	case $reply in
