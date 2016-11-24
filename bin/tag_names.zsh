4296c14d57cd183f66fc3344a21643e9eae2d6b6
Caleb Maclennan <caleb@alerque.com>
2016-11-24 16:31:02 +0300

    Refactor as function and show diffs by character

--- bin/tag_names.zsh
+++ bin/tag_names.zsh
@@ -3,18 +3,24 @@
 lang=$1 ; shift
 names=$1 ; shift
 
-for source in $@; do
+function tag_name () {
+	md=$1 ; shift
+	name=$@
+	clear
+	git co -- $md
+	msg="Tag instances of name '$name' as language '$lang'"
+	perl -i -pne "s/(?<!\})$name(?!\})/\\\\lang$lang{$name}/g if ! /^(\[\^\d+\]|#+ )/" -- $md
+	git add -- $md
+	git --no-pager diff --cached -U0 --word-diff=color --word-diff-regex=. --minimal --ignore-all-space -- $md |
+		grep -v '@@'
+	git diff-index --quiet --cached HEAD && continue 1
+	read -q "?$msg? (y/n)" || continue 1
+	git commit -m "[auto] $msg"
+}
+
+for file in $@; do
 	perl -e 'print sort { length($b) <=> length($a) } <>' < $names |
 		while read name; do
-			clear
-			git co -- $source
-			msg="Tag instances of name '$name' as language '$lang'"
-			perl -i -pne "s/(?<!\})$name(?!\})/\\\\lang$lang{$name}/g if ! /^(\[\^\d+\]|#+ )/" -- $source
-			git add -- $source
-			git --no-pager diff --cached -U0 --word-diff=color --minimal --ignore-all-space -- $source |
-				grep -v '@@'
-			git diff-index --quiet --cached HEAD && continue 1
-			read -q "?$msg? (y/n)" || continue 1
-			git commit -m "[auto] $msg"
+			tag_name $file $name
 		done
 done
