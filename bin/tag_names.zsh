0b0267d73f7df14bbbecbd559a8c5d2606a97438
Caleb Maclennan <caleb@alerque.com>
2016-11-25 19:07:02 +0300

    Keep (local) track of rejected language tagging

--- bin/tag_names.zsh
+++ bin/tag_names.zsh
@@ -3,11 +3,17 @@
 lang=$1 ; shift
 names=$1 ; shift
 
+touch rejects.txt
+
+function reject () {
+	echo $@ > rejects.txt
+}
+
 function tag_name () {
 	md=$1 ; shift
 	name=$@
 	clear
-	git co -- $md
+	git co HEAD -- $md
 	git reset
 	msg="Tag instances of name '$name' as language '$lang'"
 	perl -i -pne "s/(?<!\{)$name(?!\})/\\\\lang$lang{$name}/g if ! /^(\[\^\d+\]|#+ )/" -- $md
@@ -15,17 +21,18 @@ function tag_name () {
 	git --no-pager diff --cached -U0 --word-diff=color --word-diff-regex=. --minimal --ignore-all-space -- $md |
 		grep -v '@@'
 	git diff-index --quiet --cached HEAD && continue 1
-	read -q "?$msg? (y/n)" || continue 1
+	read -q "?$msg? (y/n)" || reject $name && continue 1
 	git commit -m "[auto] $msg"
 }
 
 # First catch instances of whole names
 for file in $@; do
 	perl -e 'print sort { length($b) <=> length($a) } <>' < $names |
+		grep -vxf rejects.txt |
 		while read name; do
 			tag_name $file $name
 		done
-	git co -- $file
+	git co HEAD -- $file
 	git reset
 done
 
@@ -33,9 +40,10 @@ done
 for file in $@; do
 	perl -ne 's/ /\n/g;print if /^.{3,}/' < $names |
 		perl -e 'print sort { length($b) <=> length($a) } <>' |
+		grep -vxf rejects.txt |
 		while read name; do
 			tag_name $file $name
 		done
-	git co -- $file
+	git co HEAD -- $file
 	git reset
 done
