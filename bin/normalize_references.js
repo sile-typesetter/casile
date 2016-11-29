f9d2d5b6a0076e19cdd3c1cd779843f18f5819b7
Caleb Maclennan <caleb@alerque.com>
2016-11-29 18:38:32 +0300

    Process chains of references in single lumps

--- bin/normalize_references.js
+++ bin/normalize_references.js
@@ -20,20 +20,41 @@ String.prototype.replaceSplice = function (start, end, string) {
   return this.substr(0,start) + string + this.substr(end);
 };
 
+function isChained(context, ref, line, offset) {
+  var dist = ref.indices[0] - context.indices[1];
+  if ( dist >= 4 ) { return false; }
+  var connector = line.substr(context.indices[1] + offset, dist);
+  return (/^[;, ]+$/).test(connector);
+}
+
+function mergeRefs(a, b) {
+  return {
+    osis: a.osis + "," + b.osis,
+    translations: a.translations,
+    indices: [ a.indices[0], b.indices[1] ]
+  };
+}
+
 function process_line (line) {
   line = line + "\n"; // restore that which readline rightfully stole
   if (!line.match(/^(#|\[\^\d+\]:) /)) { // skip footnotes and headings for now
     var offset = 0;
-    var lastEnd = 0;
-    var lastOsis = null;
+    var previousOffset = 0;
+    var context = null;
     var refs = bcv.parse(line).osis_and_indices();
     refs.forEach(function(ref) {
-      var context = ( lastEnd + 4 ) >= ref.indices[0] ? lastOsis : null;
-      var pretty = formatter('yc-long', ref.osis, context);
-      lastEnd = ref.indices[1];
-      lastOsis = ref.osis;
-      line = line.replaceSplice(ref.indices[0] + offset, ref.indices[1] + offset, pretty); 
-      offset += ref.indices[0] - ref.indices[1] + pretty.length;
+      if ( context && isChained(context, ref, line, offset) ) {
+        var mref = mergeRefs(context, ref);
+        ref = mref;
+        offset -= previousOffset;
+      } else {
+        previousOffset = 0;
+      }
+      var pretty = formatter('yc-long', ref.osis);
+      line = line.replaceSplice(ref.indices[0] + offset, ref.indices[1] + offset + previousOffset, pretty);
+      previousOffset = ref.indices[0] - ref.indices[1] + pretty.length;
+      offset += previousOffset;
+      context = ref;
     });
   }
   process.stdout.write(line);
