231d4d50f6a4580bb8fcab974efba9938151bc2e
Caleb Maclennan <caleb@alerque.com>
2016-12-19 20:35:03 +0300

    Add folio fram to A5trim and normalize with other layouts

--- layout-a5trim.lua
+++ layout-a5trim.lua
@@ -1,20 +1,39 @@
-local book = SILE.require("classes/book");
-book:loadPackage("masters")
-book:defineMaster({ id = "right", firstContentFrame = "content", frames = {
-  content = { left = "22.5mm", right = "100%pw-15mm", top = "20mm", bottom = "top(footnotes)" },
-  runningHead = { left = "left(content)", right = "right(content)", top = "top(content)-8mm", bottom = "top(content)-2mm" },
-  footnotes = { left = "left(content)", right = "right(content)", height = "0", bottom = "100%ph-15mm" }
-}})
-book:defineMaster({ id = "left", firstContentFrame = "content", frames = {} })
-book:loadPackage("twoside", { oddPageMaster = "right", evenPageMaster = "left" });
-book:mirrorMaster("right", "left")
-SILE.call("switch-master-one-page", { id = "right"  })
+local class = SILE.documentState.documentClass
 
-local oldImprintFont = SILE.Commands["imprint:font"]
-SILE.registerCommand("imprint:font", function (options, content)
-  options.size = options.size or "8.5pt"
-  oldImprintFont(options, content)
-end)
+class:defineMaster({
+    id = "right",
+    firstContentFrame = "content",
+    frames = {
+      content = {
+        left = "22.5mm",
+        right = "100%pw-15mm",
+        top = "20mm",
+        bottom = "top(footnotes)"
+      },
+      runningHead = {
+        left = "left(content)",
+        right = "right(content)",
+        top = "top(content)-8mm",
+        bottom = "top(content)-2mm"
+      },
+      footnotes = {
+        left = "left(content)",
+        right = "right(content)",
+        height = "0",
+        bottom = "100%ph-15mm"
+      },
+      folio = {
+        left = "left(content)",
+        right = "right(content)",
+        top = "100%ph-13mm",
+        height = "6mm"
+      }
+    }
+  })
+class:mirrorMaster("right", "left")
+SILE.call("switch-master-one-page", { id = "right" })
+
+setCommandDefaults("imprint:font", { size = "8.5pt" })
 
 SILE.registerCommand("href", function (options, content)
   SILE.process(content)
