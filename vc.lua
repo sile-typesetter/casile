90f50bf83c4bc02f8826c89412c96fc4a67467d8
Caleb Maclennan <caleb@alerque.com>
2016-12-20 16:37:37 +0300

    Cleanup class to not reset typesetter in header and simplify code

--- vc.lua
+++ vc.lua
@@ -3,14 +3,11 @@ local book = SILE.require("classes/book")
 local plain = SILE.require("classes/plain")
 local vc = book { id = "vc" }
 
-vc:loadPackage("folio")
-
 vc.endPage = function (self)
   vc:moveTocNodes()
 
   if (not SILE.scratch.headers.skipthispage) then
     SILE.settings.pushState()
-    SILE.settings.reset()
     if (vc:oddPage() and SILE.scratch.headers.right) then
       SILE.typesetNaturally(SILE.getFrame("runningHead"), function ()
         SILE.call("output-right-running-head")
@@ -21,9 +18,8 @@ vc.endPage = function (self)
       end)
     end
     SILE.settings.popState()
-  else
-    SILE.scratch.headers.skipthispage = false
   end
+  SILE.scratch.headers.skipthispage = false
   return plain.endPage(book)
 end
 
