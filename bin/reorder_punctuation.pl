a6275d7f4bc5713c631b76fa778b336d650890ad
Caleb Maclennan <caleb@alerque.com>
2016-12-28 14:37:02 +0300

    Add rules about closing marks to punctuation ordering

--- bin/reorder_punctuation.pl
+++ bin/reorder_punctuation.pl
@@ -6,5 +6,7 @@ use open ':std', ':encoding(UTF-8)';
 while (<>) {
   # Place footnote markers after any trailing punctuation.
   s/(?<!^)(\[\^\d+\])([\p{P}]+)/\2\1/g;
+  # Move trailing punction inside double quotes
+  s/”([\.?!])/\1”/g;
   print;
 }
