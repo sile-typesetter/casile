c7e31e564df02760c7e97a99dbcb37226f85149a
Caleb Maclennan <caleb@alerque.com>
2017-01-05 20:39:03 +0300

    Use special spaces around initials, fixes viachristus/teolojiye_giris#125

--- bin/reorder_punctuation.pl
+++ bin/reorder_punctuation.pl
@@ -8,5 +8,7 @@ while (<>) {
   s/(?<!^)(\[\^\d+\])([\p{P}]+)/\2\1/g;
   # Move trailing punction inside double quotes
   s/”([\.?!])/\1”/g;
+  # Use thin space between proper name initials
+  s/(?<=\p{Lu}\.) (\p{Lu}\.) (?=\p{Lu})/ \1 /g;
   print;
 }
