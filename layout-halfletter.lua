c9169dc990d71bfe816cd28c7563a327a5de4b8b
Caleb Maclennan <caleb@alerque.com>
2017-02-10 12:55:31 +0300

    Bring Halfletter layout into line with class methods

--- layout-halfletter.lua
+++ layout-halfletter.lua
@@ -1,13 +1,37 @@
-local book = SILE.require("classes/book");
-book:loadPackage("masters")
-book:defineMaster({ id = "right", firstContentFrame = "content", frames = {
-  content = { left = "22.5mm", right = "100%pw-15mm", top = "20mm", bottom = "top(footnotes)" },
-  runningHead = { left = "left(content)", right = "right(content)", top = "top(content)-8mm", bottom = "top(content)" },
-  footnotes = { left = "left(content)", right = "right(content)", height = "0", bottom = "100%ph-15mm" }
-}})
-book:defineMaster({ id = "left", firstContentFrame = "content", frames = {} })
-book:loadPackage("twoside", { oddPageMaster = "right", evenPageMaster = "left" });
-book:mirrorMaster("right", "left")
+local class = SILE.documentState.documentClass
+
+SILE.documentState.paperSize = SILE.paperSizeParser("halfletter")
+SILE.documentState.orgPaperSize = SILE.documentState.paperSize
+
+class:defineMaster({
+    id = "right",
+    firstContentFrame = "content",
+    frames = {
+      content = {
+        left = "22.5mm",
+        right = "100%pw-15mm",
+        top = "20mm",
+        bottom = "top(footnotes)"
+      },
+      runningHead = {
+        left = "left(content)",
+        right = "right(content)",
+        top = "top(content)-8mm",
+        bottom = "top(content)"
+      },
+      footnotes = {
+        left = "left(content)",
+        right = "right(content)",
+        height = "0",
+        bottom = "100%ph-15mm"
+      }
+    }
+  })
+class:mirrorMaster("right", "left")
 SILE.call("switch-master-one-page", { id = "right" })
 
-SILE.require("crop")
+if SILE.documentState.documentClass.options.crop() == "true" then SILE.require("crop") end
+
+SILE.registerCommand("href", function (options, content)
+  SILE.process(content)
+end)
