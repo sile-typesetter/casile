1e31f9bda61c6421a3788f28d75ba45c4838b1bd
Caleb Maclennan <caleb@alerque.com>
2017-02-11 11:23:08 +0300

    Handle case of no identifiers at all in meta data

--- bin/isbn_format.py
+++ bin/isbn_format.py
@@ -11,11 +11,12 @@ yamldata = yaml.load(metadata)
 
 identifier = {}
 
-for id in yamldata["identifier"]:
-    if "key" in id:
-        isbnlike = isbnlib.get_isbnlike(id["text"])[0]
-        if isbnlib.is_isbn13(isbnlike):
-            identifier[id["key"]] = isbnlib.EAN13(isbnlike)
+if "identifier" in yamldata:
+    for id in yamldata["identifier"]:
+        if "key" in id:
+            isbnlike = isbnlib.get_isbnlike(id["text"])[0]
+            if isbnlib.is_isbn13(isbnlike):
+                identifier[id["key"]] = isbnlib.EAN13(isbnlike)
 
 isbn = identifier[sys.argv[2]] if sys.argv[2] in identifier else "9786056644504"
 
