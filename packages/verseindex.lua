9d0ea605dce0679712baba29e1301f4f09dd230b
Caleb Maclennan <caleb@alerque.com>
2017-02-14 19:53:34 +0300

    Change the verse index header again
    Per other books

--- packages/verseindex.lua
+++ packages/verseindex.lua
@@ -64,7 +64,7 @@ local init = function (self)
   end)
 
   SILE.registerCommand("tableofverses", function (options, content)
-    SILE.call("chapter", { numbering = "false" }, { "Ek: Ayet Referansları" })
+    SILE.call("chapter", { numbering = "false" }, { "Ek: Ayet Referans İndeksi" })
     SILE.call("cabook:seriffont", { size = "0.95em" })
     local refshash = {}
     local lastbook = nil
