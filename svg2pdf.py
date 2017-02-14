03517b2b35f5a55b419d9ca2c0d1a47b2703559b
Caleb Maclennan <caleb@alerque.com>
2017-02-14 21:45:03 +0300

    Call make from Pandoc filter rather than re-inventing an mtime mechanism

--- svg2pdf.py
+++ svg2pdf.py
@@ -2,9 +2,8 @@
 """
 Pandoc filter to convert svg files to pdf as suggested at:
 https://github.com/jgm/pandoc/issues/265#issuecomment-27317316
+Modified to use make at build time
 """
-
-__author__ = "Jerome Robert"
 
 import mimetypes
 import subprocess
@@ -13,8 +12,8 @@ import sys
 from pandocfilters import toJSONFilter, Image
 
 fmt_to_option = {
-    "sile": ("--export-pdf","pdf"),
-    "docx": ("--export-png", "png"),
+    "sile": "pdf",
+    "docx": "png",
 }
 
 def svg_to_any(key, value, fmt, meta):
@@ -26,18 +25,13 @@ def svg_to_any(key, value, fmt, meta):
        else:
            attrs, alt, [src, title] = value
        mimet,_ = mimetypes.guess_type(src)
-       option = fmt_to_option.get(fmt)
-       if mimet == 'image/svg+xml' and option:
+       targetfmt = fmt_to_option.get(fmt)
+       if mimet == 'image/svg+xml' and targetfmt:
            base_name,_ = os.path.splitext(src)
-           eps_name = base_name + "." + option[1]
-           try:
-               mtime = os.path.getmtime(eps_name)
-           except OSError:
-               mtime = -1
-           if mtime < os.path.getmtime(src):
-               cmd_line = ['inkscape', '--export-ignore-filters', option[0], eps_name, src]
-               sys.stderr.write("Running %s\n" % " ".join(cmd_line))
-               subprocess.call(cmd_line, stdout=sys.stderr.fileno())
+           eps_name = base_name + "." + targetfmt
+           cmd_line = ['make', eps_name]
+           sys.stderr.write("Running %s\n" % " ".join(cmd_line))
+           subprocess.call(cmd_line, stdout=sys.stderr.fileno())
            if attrs:
                return Image(attrs, alt, [eps_name, title])
            else:
