a568d8db322a6d2085d90326d791bfbaa24ab875
Caleb Maclennan <caleb@alerque.com>
2017-02-23 21:42:59 +0300

    Don't load up the verse index code unless we use it (keeps the special nodes clean)

--- cabook.lua
+++ cabook.lua
@@ -6,7 +6,11 @@ cabook:declareOption("crop", "true")
 cabook:declareOption("background", "true")
 cabook:declareOption("verseindex", "false")
 
-cabook:loadPackage("verseindex", CASILE.casiledir)
+if cabook.options.verseindex() == "true" then
+  cabook:loadPackage("verseindex", CASILE.casiledir)
+else
+  SILE.registerCommand("markverse", function (options, content) end)
+end
 
 cabook.endPage = function (self)
   cabook:moveTocNodes()
