994ac591084db71628222bf7837df5c355a14ba2
Caleb Maclennan <caleb@alerque.com>
2017-02-24 19:42:21 +0300

    Add a realy basic package to parse Markdown into normal content

---
+++ packages/markdown.lua
@@ -0,0 +1,16 @@
+SILE.processMarkdown = function (content)
+  local lunamark = require("lunamark")
+  local reader = lunamark.reader.markdown
+  local writer = lunamark.writer.ast.new()
+  local parse = reader.new(writer)
+  local output = parse(tostring(content[1]))
+  SILE.process(output)
+end
+
+SILE.registerCommand("processMarkdown", function (option, content)
+  SILE.processMarkdown(SU.contentToString(content))
+end)
+
+SILE.registerCommand("emphasis", function(options, content)
+  SILE.call("em", options, content)
+end)
