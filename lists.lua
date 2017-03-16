ecc206772c502102b2d01c526036ad0c79f45879
Caleb Maclennan <caleb@alerque.com>
2017-03-16 17:43:03 +0300

    Setup nested indents for nested lists

--- lists.lua
+++ lists.lua
@@ -1,16 +1,23 @@
 SILE.require("packages.counters")
 SILE.scratch.liststyle = nil
 
+local nestedlist = 0
+local listarealskip = nil
+
 SILE.registerCommand("listarea", function (options, content)
+  nestedlist = nestedlist + 1
+  if nestedlist == 1 then listarealskip = SILE.settings.get("document.parindent").width end
   SILE.scratch.liststyle = options.numberstyle
   SILE.call("set-counter", { id = "listarea", value = 1, display = SILE.scratch.liststyle or "arabic" })
+  SU.debug("casile", "nested count", nestedlist, listarealskip)
   SILE.settings.temporarily(function ()
-    SILE.settings.set("document.rskip", SILE.nodefactory.newGlue(options.rskip or tostring(SILE.settings.get("document.parindent").width)))
-    SILE.settings.set("document.lskip", SILE.nodefactory.newGlue(options.lskip or tostring(SILE.settings.get("document.parindent").width)))
     SILE.settings.set("document.parindent", SILE.nodefactory.zeroGlue)
+    SILE.settings.set("document.lskip", SILE.nodefactory.newGlue(options.lskip or tostring(listarealskip * nestedlist)))
+    SILE.settings.set("document.rskip", SILE.nodefactory.newGlue(options.rskip or 0))
     SILE.process(content)
   end)
   SILE.call("noindent")
+  nestedlist = nestedlist - 1
 end)
 
 SILE.registerCommand("listitem", function (options, content)
