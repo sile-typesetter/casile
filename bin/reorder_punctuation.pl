ec54c879f291f0f7b3a4568c076c5d29c1601adc
Caleb Maclennan <caleb@alerque.com>
2017-05-17 11:57:54 +0300

    Include commas in trailing punctuation to be inside parenthesis

--- bin/reorder_punctuation.pl
+++ bin/reorder_punctuation.pl
@@ -7,7 +7,7 @@ while (<>) {
   # Place footnote markers after any trailing punctuation.
   s/(?<!^)(\[\^\d+\])([\p{P}]+)/\2\1/g;
   # Move trailing punction inside double quotes
-  s/”([\.?!])/\1”/g;
+  s/”([,\.?!])/\1”/g;
   # Use thin space between proper name initials
   s/(?<=\p{Lu}\.) (\p{Lu}\.) (?=\p{Lu})/ \1 /g;
   print;
