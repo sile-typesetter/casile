40ed77cb832bf4c65e488fe3a73465fd12684a3a
Caleb Maclennan <caleb@alerque.com>
2017-05-29 18:59:57 +0300

    Backport nested list handling from *Kurtuluş* project code

--- lists.lua
+++ lists.lua
@@ -2,18 +2,19 @@ SILE.require("packages.counters")
 SILE.scratch.liststyle = nil
 
 local nestedlist = 0
+local liststyles = {}
 local listarealskip = nil
 
 SILE.registerCommand("listarea", function (options, content)
   nestedlist = nestedlist + 1
   if nestedlist == 1 then listarealskip = SILE.settings.get("document.parindent").width end
-  SILE.scratch.liststyle = options.numberstyle
-  SILE.call("set-counter", { id = "listarea", value = 1, display = SILE.scratch.liststyle or "arabic" })
-  SU.debug("casile", "nested count", nestedlist, listarealskip)
+  liststyles[nestedlist] = { options.numberstyle }
+  SILE.call("set-counter", { id = "listarea" .. nestedlist, value = 1, display = options.numberstyle or "arabic" })
   SILE.settings.temporarily(function ()
     SILE.settings.set("document.parindent", SILE.nodefactory.zeroGlue)
-    SILE.settings.set("document.lskip", SILE.nodefactory.newGlue(options.lskip or tostring(listarealskip * nestedlist)))
-    SILE.settings.set("document.rskip", SILE.nodefactory.newGlue(options.rskip or 0))
+    local factor = nestedlist == 1 and 0 or nestedlist / 2
+    SILE.settings.set("document.lskip", SILE.nodefactory.newGlue(options.lskip or tostring(listarealskip + listarealskip * factor)))
+	SILE.settings.set("document.rskip", SILE.nodefactory.newGlue(options.rskip or 0))
     SILE.process(content)
   end)
   SILE.call("noindent")
@@ -21,19 +22,17 @@ SILE.registerCommand("listarea", function (options, content)
 end)
 
 SILE.registerCommand("listitem", function (options, content)
-  local lskip = SILE.settings.get("document.lskip").width
   local markerwidth = SILE.length.parse("1.5em")
-  SILE.call("kern", { width = lskip })
-  SILE.call("kern", { width = markerwidth:negate() })
+  SILE.call("kern", { width = tostring(markerwidth:negate()) })
   SILE.call("rebox", { width = tostring(markerwidth) }, function ()
-    if SILE.scratch.liststyle then
-      SILE.call("show-counter", { id = "listarea" })
+    if liststyles[nestedlist][1] then
+      SILE.call("show-counter", { id = "listarea" .. nestedlist })
       SILE.typesetter:typeset(".")
     else
       SILE.typesetter:typeset("•")
     end
   end)
-  SILE.call("increment-counter",  { id = "listarea" })
+  SILE.call("increment-counter",  { id = "listarea" .. nestedlist })
   SILE.process(content)
-  SILE.call("smallskip")
+  if nestedlist == 1 then SILE.call("smallskip") else SILE.call("par") end
 end)
