d60c00bdb2cbab43b637143bc82aefa432510cd7
Caleb Maclennan <caleb@alerque.com>
2019-01-03 17:11:17 +0300

    Allow a callback to do fancy things post markdown parsing

--- packages/markdown.lua
+++ packages/markdown.lua
@@ -1,14 +1,15 @@
-SILE.processMarkdown = function (content)
+SILE.processMarkdown = function (content, callback)
+  local callback = callback or function (...) return ... end
   local lunamark = require("lunamark")
   local reader = lunamark.reader.markdown
   local writer = lunamark.writer.ast.new()
   local parse = reader.new(writer)
-  local output = parse(tostring(content[1]))
+  local output = callback(parse(tostring(content[1])))
   SILE.process(output)
 end
 
 SILE.registerCommand("processMarkdown", function (option, content)
-  SILE.processMarkdown(SU.contentToString(content))
+  SILE.processMarkdown(SU.contentToString(content), options.callback)
 end)
 
 SILE.registerCommand("emphasis", function (options, content)
