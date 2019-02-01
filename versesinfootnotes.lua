006c522b267baf3a1b7a675aa220f0f8ae85299c
Caleb Maclennan <caleb@alerque.com>
2019-02-01 14:10:40 +0300

    Use link titles not text for full reference

--- versesinfootnotes.lua
+++ versesinfootnotes.lua
@@ -3,11 +3,13 @@ local yaml = require("yaml")
 Pandoc = function (document)
   local versedata = yaml.loadpath(document.meta.versedatafile)
 
+  document.meta.versedatafile = nil
+
   document.blocks = pandoc.walk_block(pandoc.Div(document.blocks), {
       Note = function (element)
         return pandoc.walk_inline(element, {
             Link = function (element)
-              local verse = element.content[1].text
+              local verse = pandoc.pipe("./casile/bin/normalize_references.js", {}, element.title):gsub("^%s*(.-)%s*$", "%1")
               local versecontent = tostring(versedata[verse]):gsub("\n", "")
               return versecontent and { pandoc.Strong(element), pandoc.Space(), pandoc.Str(versecontent)} or element
             end
