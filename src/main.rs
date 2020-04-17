1c9fd9d7beb371af75e2c54f6ed24734539bb3bf
Caleb Maclennan <caleb@alerque.com>
2020-04-18 00:23:37 +0300

    chore(cli): Pars args for flags vs. subcommands

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,31 @@ use structopt::StructOpt;
 /// The CaSILE command line interface, publishing automation level wizard.
 #[derive(StructOpt)]
 struct Cli {
+    // #[structopt(short, long)]
+    // version: [ version = env!("VERGEN_SEMVER") ],
+
     /// Activate debug mode
     #[structopt(short, long)]
     debug: bool,
 
-    /// Output verbose feedback where possible
+    /// Outputs verbose feedback where possible
     #[structopt(short, long)]
     verbose: bool,
 
-    /// Primary CaSILE command (try make)
-    command: String,
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(StructOpt)]
+enum Command {
+    /// Executes a make target
+    Make {
+        target: String
+    }
 }
 
 fn main() {
     let args = Cli::from_args();
     println!("Insert magic potion!");
+    println!("Build SHA: {}", env!("VERGEN_SHA_SHORT"));
 }
