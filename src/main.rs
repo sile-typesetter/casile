b47b9800419a1dae3ddbbb0160889d11d1730487
Caleb Maclennan <caleb@alerque.com>
2020-04-27 09:24:35 +0300

    style(cli): Reformat code with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use structopt::StructOpt;
-use structopt::clap::AppSettings;
-use std::path;
-use std::vec;
+use git2::Repository;
 use std::fs;
 use std::io;
 use std::io::{Error, ErrorKind};
-use git2::Repository;
+use std::path;
+use std::vec;
+use structopt::clap::AppSettings;
+use structopt::StructOpt;
 
 /// The command line interface to the CaSILE toolkit, a book publishing
 /// workflow employing SILE and other wizardry
@@ -13,7 +13,6 @@ use git2::Repository;
 #[structopt(version = env!("VERGEN_SEMVER"))]
 #[structopt(setting = AppSettings::InferSubcommands)]
 struct Cli {
-
     /// Activate debug mode
     #[structopt(short, long, env = "DEBUG")]
     debug: bool,
@@ -24,37 +23,29 @@ struct Cli {
 
     #[structopt(subcommand)]
     subcommand: Subcommand,
-
 }
 
 #[derive(StructOpt)]
 enum Subcommand {
-
     /// Executes a make target
     Make {
-
         /// Target as defined in CaSILE makefile
         target: Vec<String>,
-
     },
 
     /// Configure a book repository
     Setup {
-
         /// Path to project repository
         #[structopt(parse(from_os_str), default_value = "./")]
         path: path::PathBuf,
-
     },
 
     /// Pass through other commands to shell
     #[structopt(external_subcommand)]
     Other(Vec<String>),
-
 }
 
-fn main () -> io::Result<()> {
-
+fn main() -> io::Result<()> {
     let args = Cli::from_args();
 
     if args.debug {
@@ -66,38 +57,33 @@ fn main () -> io::Result<()> {
     }
 
     return match args.subcommand {
-        Subcommand::Make{ target } => make(target),
-        Subcommand::Setup{ path } => setup(path),
+        Subcommand::Make { target } => make(target),
+        Subcommand::Setup { path } => setup(path),
         _a => shell(),
-    }
-
+    };
 }
 
-fn make (_target: vec::Vec<String>) -> io::Result<()> {
-
+fn make(_target: vec::Vec<String>) -> io::Result<()> {
     println!("Make make make sense or I’ll make you make makefiles.");
 
     Ok(())
 }
 
-fn setup (path: path::PathBuf) -> io::Result<()> {
-
+fn setup(path: path::PathBuf) -> io::Result<()> {
     let metadata = fs::metadata(&path)?;
 
     return match metadata.is_dir() {
         true => match Repository::open(path) {
-            Ok(_repo) => Ok(
-                println!("Run setup, “They said you were this great colossus!”")
-            ),
+            Ok(_repo) => Ok(println!(
+                "Run setup, “They said you were this great colossus!”"
+            )),
             Err(_error) => Err(Error::new(ErrorKind::InvalidInput, "Not a git repo!")),
         },
         false => Err(Error::new(ErrorKind::InvalidInput, "Not a dir, Frank!")),
-    }
-
+    };
 }
 
-fn shell () -> io::Result<()> {
-
+fn shell() -> io::Result<()> {
     println!("Ship all this off to the shell, maybe they can handle it.");
 
     Ok(())
