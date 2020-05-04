8e6d948285926d592d2d8fa054d79328404c8a45
Caleb Maclennan <caleb@alerque.com>
2020-05-05 01:39:11 +0300

    refactor(cli): Make cargo bulid take care of creating output dir

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ extern crate vergen;
 use clap::IntoApp;
 use clap_generate::generate_to;
 use clap_generate::generators::{Bash, Elvish, Fish, PowerShell, Zsh};
-use std::env;
+use std::{env, fs};
 use vergen::{generate_cargo_keys, ConstantsFlags};
 
 include!("src/cli.rs");
@@ -16,15 +16,16 @@ fn main() {
     // Generate the 'cargo:' key output
     generate_cargo_keys(flags).expect("Unable to generate the cargo keys!");
 
-    let outdir = match env::var_os("OUT_DIR") {
-        None => return,
-        Some(outdir) => outdir,
-    };
+    // let outdir = env::var_os("OUT_DIR").unwrap();
+    let profile = env::var("PROFILE").unwrap();
+    let completionsdir = format!("target/{}/completions", profile);
+    fs::create_dir(&completionsdir).unwrap();
+    println!("CoDi {}", &completionsdir);
     let mut app = Cli::into_app();
     let bin_name = "casile";
-    generate_to::<Bash, _, _>(&mut app, bin_name, &outdir);
-    generate_to::<Elvish, _, _>(&mut app, bin_name, &outdir);
-    generate_to::<Fish, _, _>(&mut app, bin_name, &outdir);
-    generate_to::<PowerShell, _, _>(&mut app, bin_name, &outdir);
-    generate_to::<Zsh, _, _>(&mut app, bin_name, &outdir);
+    generate_to::<Bash, _, _>(&mut app, bin_name, &completionsdir);
+    generate_to::<Elvish, _, _>(&mut app, bin_name, &completionsdir);
+    generate_to::<Fish, _, _>(&mut app, bin_name, &completionsdir);
+    generate_to::<PowerShell, _, _>(&mut app, bin_name, &completionsdir);
+    generate_to::<Zsh, _, _>(&mut app, bin_name, &completionsdir);
 }
