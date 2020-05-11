8896dbf0fc26a44e47ec1d8d784dc3da3e190fbc
Caleb Maclennan <caleb@alerque.com>
2020-05-11 13:17:26 +0300

    chore(cli): Process status checks in parallel threads

--- src/status/mod.rs
+++ src/status/mod.rs
@@ -1,6 +1,7 @@
 use crate::i18n::LocalText;
 use git2::Repository;
-use std::{env, error, path, result};
+use std::sync::{Arc, RwLock};
+use std::{env, error, path, result, thread};
 
 type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
@@ -13,10 +14,14 @@ pub fn run() -> Result<()> {
 
 /// Evaluate whether this project is pis_setup()?roperly configured
 pub fn is_setup() -> Result<bool> {
-    let mut checks: Vec<bool> = Vec::new();
-    let is_repo = is_repo(env::current_dir()?)?;
-    checks.push(is_repo);
-    let ret = checks.iter().all(|&v| v);
+    let results = Arc::new(RwLock::new(Vec::new()));
+    rayon::scope(|s| {
+        s.spawn(|_| {
+            let repo = is_repo(&env::current_dir().unwrap()).unwrap();
+            results.write().unwrap().push(repo);
+        });
+    });
+    let ret = results.read().unwrap().iter().all(|&v| v);
     eprintln!(
         "{}",
         LocalText::new(if ret { "status-good" } else { "status-bad" }).fmt()
@@ -25,7 +30,7 @@ pub fn is_setup() -> Result<bool> {
 }
 
 /// Are we in a git repo?
-pub fn is_repo(path: path::PathBuf) -> Result<bool> {
+pub fn is_repo(path: &path::PathBuf) -> Result<bool> {
     let ret = Repository::discover(path).is_ok();
     eprintln!(
         "{} {}",
