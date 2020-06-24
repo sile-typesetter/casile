5e83123da516e66c7704a068145e0bdfd8e6456b
Caleb Maclennan <caleb@alerque.com>
2020-06-24 18:02:24 +0300

    fix: Fix order so we can't possibly measure an empty stack

--- casile.lua
+++ casile.lua
@@ -669,15 +669,15 @@ SILE.registerCommand("dropcap", function (_, content)
 end)
 
 SILE.registerCommand("requireSpace", function (options, content)
-	local required = SILE.length(options.height or 1)
-	SILE.typesetter:leaveHmode()
-	SILE.Commands["hbox"]({}, content)
-	table.remove(SILE.typesetter.state.nodes) -- steal it back
-	local heightOfPageSoFar = SILE.pagebuilder.collateVboxes(SILE.typesetter.state.outputQueue).height
-	local heightOfFrame = SILE.typesetter.frame:height()
-	if heightOfFrame - heightOfPageSoFar < required then
-		SILE.call("supereject")
-	end
+  local required = SILE.length(options.height or 0)
+  SILE.typesetter:leaveHmode()
+  SILE.call("hbox", {}, content) -- push content we want to fit
+  local heightOfPageSoFar = SILE.pagebuilder:collateVboxes(SILE.typesetter.state.outputQueue).height
+  local heightOfFrame = SILE.typesetter.frame:height()
+  table.remove(SILE.typesetter.state.nodes) -- steal it back
+  if heightOfFrame - heightOfPageSoFar < required then
+    SILE.call("supereject")
+  end
 end)
 
 SILE.registerUnit("%pmed", { relative = true, definition = function (v)
