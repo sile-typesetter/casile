7cab7b4ea5fa5b4b10288164261107882dde9095
Caleb Maclennan <caleb@alerque.com>
2021-01-02 13:25:36 +0300

    chore(rules): Assume PATH is working to reach scripts
    Easier to override than full hard coded paths...

--- rules/rules.mk
+++ rules/rules.mk
@@ -729,7 +729,7 @@ $(PLAYEPUBS): %.epub: $$(call isbntouid,$$*).epub | $(require_pubdir)
 	rm -f $(PUBDIR)/$<
 
 %-$(_app).info: %-$(_app)-$(_print).toc %-$(_app)-$(_print).pdf %-manifest.yml | $(require_pubdir)
-	$(CASILEDIR)scripts/toc2breaks.lua $* $(filter %-$(_app)-$(_print).toc,$^) $(filter %-manifest.yml,$^) $@ |
+	toc2breaks.lua $* $(filter %-$(_app)-$(_print).toc,$^) $(filter %-manifest.yml,$^) $@ |
 		while read range out; do
 			$(PDFTK) $(filter %-$(_app)-$(_print).pdf,$^) cat $$range output $$out
 			ln -f $$out $(PUBDIR)/$$out
@@ -1313,7 +1313,7 @@ INTERMEDIATES += *-$(_barcode).*
 			--scale=5 \
 			--barcode=69 \
 			--height=30 \
-			--data=$(shell $(CASILEDIR)scripts/isbn_format.py $< paperback) |\
+			--data=$(shell isbn_format.py $< paperback) |\
 		$(SED) -e 's/Helvetica\( Regular\)\?/TeX Gyre Heros/g' \
 		> $@
 
@@ -1323,11 +1323,11 @@ INTERMEDIATES += *-$(_barcode).*
 		$< \
 		-bordercolor white -border 10 \
 		-font Hack-Regular -pointsize 36 \
-		label:"ISBN $(shell $(CASILEDIR)scripts/isbn_format.py $*-manifest.yml paperback mask)" +swap -gravity Center -append \
+		label:"ISBN $(shell isbn_format.py $*-manifest.yml paperback mask)" +swap -gravity Center -append \
 		-bordercolor white -border 0x10 \
 		-resize $(call scale,1200)x \
 		$@
-	if [[ $(shell $(CASILEDIR)scripts/isbn_format.py $*-manifest.yml paperback) == 9786056644504 ]]; then
+	if [[ $(shell isbn_format.py $*-manifest.yml paperback) == 9786056644504 ]]; then
 		$(MAGICK) \
 			$(MAGICKARGS) \
 			$@ \
@@ -1349,9 +1349,9 @@ $(STATSSOURCES): %-stats:
 repository-lastcommit.ts: $$(newcommits)
 	touch -d $$($(GIT) log -n1 --format=%cI) $@
 
-repository-worklog.sqlite: $(CASILEDIR)scripts/worklog.zsh repository-lastcommit.ts
+repository-worklog.sqlite: repository-lastcommit.ts
 	$(SQLITE3) $@ 'DROP TABLE IF EXISTS commits; CREATE TABLE commits (sha TEXT, author TEXT, date DATE, file TEXT, added INT, removed INT)'
-	$< | $(SQLITE3) -batch $@
+	worklog.zsh | $(SQLITE3) -batch $@
 
 WORKLOGFIELDS := sha AS 'Commit', date AS 'Date', file AS 'Filename', added AS 'Added', removed AS 'Removed'
 
