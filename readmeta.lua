3d96da33fb3d269eb77261854d46234bd8928cb0
Caleb Maclennan <caleb@alerque.com>
2021-02-05 02:17:24 +0300

    fix(metadata): Handle YAML keys that start with digits as strings

--- readmeta.lua
+++ readmeta.lua
@@ -1,5 +1,8 @@
 local yaml = require("yaml")
-yaml.configure({ load_nulls_as_nil = true })
+yaml.configure({
+	load_nulls_as_nil = true,
+	load_numeric_scalars = false,
+})
 
 return {
 	load = function (filename)
