8ad8e16b9dec194fc32a14182ae946f275f4db3f
Caleb Maclennan <caleb@alerque.com>
2022-04-01 14:15:56 +0300

    chore(mdbook): Strip out more input elements mdbook can't handle

--- pandoc-filters/strip_for_mdbook.lua
+++ pandoc-filters/strip_for_mdbook.lua
@@ -1,5 +1,13 @@
+function remove_attr (element)
+  if element.attr then
+    element.attr = pandoc.Attr()
+    return element
+  end
+end
+
 -- c.f. pandoc-filters/withoutheadinglinks.lua
 Header = function (element)
+  element = remove_attr(element)
   return pandoc.walk_block(element, {
       -- c.f. pandoc-filters/epubclean.lua
       Note = function (_)
@@ -9,13 +17,6 @@ Header = function (element)
         return element.content
       end
     })
-end
-
-function remove_attr (element)
-  if element.attr then
-    element.attr = pandoc.Attr()
-    return element
-  end
 end
 
 Inline = remove_attr
