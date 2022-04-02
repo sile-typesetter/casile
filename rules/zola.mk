8cd8cef950763e0c73f766d65a3c060e3d704527
Caleb Maclennan <caleb@alerque.com>
2022-04-02 23:57:25 +0300

    chore(pages): Make optional resources order-only

--- rules/zola.mk
+++ rules/zola.mk
@@ -3,33 +3,32 @@ ZOLAS := $(call pattern_list,$(SOURCES),.zola)
 # Unlike most other rules, the zola site stuff doesn't depend on much being generated, it mostly looks what *has been* generated and goes from there
 list_extant_resources = $(filter $1%,$(filter-out $1.zola,$(wildcard $(DISTFILES) $(DISTDIRS))))
 
-$(ZOLAS): %.zola: $(addprefix $(BUILDDIR)/%.zola/,config.toml content/_index.md templates/index.html sass/style.sass) %-epub-$(_poster).jpg $$(call list_extant_resources,$$*) | $(BUILDDIR)
-	:
+$(ZOLAS): %.zola: $(addprefix $(BUILDDIR)/%.zola/,config.toml content/_index.md templates/index.html sass/style.sass) %-epub-$(_poster).jpg | $$(call list_extant_resources,$$*) $(BUILDDIR)
 	local resourcedir="$(<D)/static"
 	rm -rf "$$resourcedir"
 	mkdir -p "$$resourcedir"
-	$(and $(filter $*%,$^),cp -a $(filter $*%,$^) "$$resourcedir")
-	$(and $(filter $*.mdbook,$^),mv $$resourcedir/$*.mdbook $$resourcedir/$(_read))
+	$(and $(filter $*%,$^ $|),cp -a $(filter $*%,$^ $|) "$$resourcedir")
+	$(and $(filter $*.mdbook,$^ $|),mv $$resourcedir/$*.mdbook $$resourcedir/$(_read))
 	rm -rf $@
 	$(ZOLA) -r "$(<D)" build -o "$@"
 
 DISTDIRS += $(ZOLAS)
 
-$(BUILDDIR)/%.zola/content/_index.md: %-manifest.yml %-epub-$(_poster).jpg $$(call list_extant_resources,$$*) | $(BUILDDIR)
+$(BUILDDIR)/%.zola/content/_index.md: %-manifest.yml %-epub-$(_poster).jpg | $$(call list_extant_resources,$$*) $(BUILDDIR)
 	mkdir -p $(@D)
 	$(ZSH) << 'EOF' # inception to break out of CaSILE’s make shell wrapper
 	exec > $@ # grey magic to capture output
 	echo "+++"
 	$(YQ) -t "{
 			\"slug\": \"$*\",
-			\"extra\": { \"coverimg\": \"$(firstword $(filter $(call pattern_list,$*,$(LAYOUTS),$(_3b)-$(_on).png),$^) $*-epub-$(_poster).jpg)\" }
+			\"extra\": { \"coverimg\": \"$(firstword $(filter $(call pattern_list,$*,$(LAYOUTS),-$(_3d)-$(_front).png),$^ $|) $*-epub-$(_poster).jpg)\" }
 		}" $<
 	echo "+++"
 	$(YQ) -r '.abstract | if . == null then "" else . end' $<
-	$(and $(filter $*.mdbook,$^),echo "- [Online oku]($(_read))")
-	$(and $(filter $*.epub,$^),echo "- EPUB olarak indir: [epub]($*.epub)")
-	$(and $(filter $*.mobi,$^),echo "- MOBI olarak indir: [mobi]($*.mobi)")
-	for pdf in $(filter $(call pattern_list,$*,$(LAYOUTS),.pdf),$^); do
+	$(and $(filter $*.mdbook,$^ $|),echo "- [Online oku]($(_read))")
+	$(and $(filter $*.epub,$^ $|),echo "- EPUB olarak indir: [epub]($*.epub)")
+	$(and $(filter $*.mobi,$^ $|),echo "- MOBI olarak indir: [mobi]($*.mobi)")
+	for pdf in $(filter $(call pattern_list,$*,$(LAYOUTS),.pdf),$^ $|); do
 		echo "- PDF olarak indir: [$${$${pdf$(hash)$*-}%.pdf}]($$pdf)"
 	done
 	EOF
