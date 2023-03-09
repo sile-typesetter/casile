8b04d35c50773ad2cd9f2beadd0edfb90047265a
Caleb Maclennan <caleb@alerque.com>
2023-03-09 18:04:27 +0300

    style: Reformat Python code with `black` defaults

--- pandoc-filters/svg2pdf.py
+++ pandoc-filters/svg2pdf.py
@@ -16,26 +16,28 @@ fmt_to_option = {
     "docx": "png",
 }
 
+
 def svg_to_any(key, value, fmt, meta):
-    if key == 'Image':
-       if len(value) == 2:
-           # before pandoc 1.16
-           alt, [src, title] = value
-           attrs = None
-       else:
-           attrs, alt, [src, title] = value
-       mimet,_ = mimetypes.guess_type(src)
-       targetfmt = fmt_to_option.get(fmt)
-       if mimet == 'image/svg+xml' and targetfmt:
-           base_name,_ = os.path.splitext(src)
-           eps_name = base_name + "." + targetfmt
-           cmd_line = ['make', eps_name]
-           sys.stderr.write("Running %s\n" % " ".join(cmd_line))
-           subprocess.call(cmd_line, stdout=sys.stderr.fileno())
-           if attrs:
-               return Image(attrs, alt, [eps_name, title])
-           else:
-               return Image(alt, [eps_name, title])
+    if key == "Image":
+        if len(value) == 2:
+            # before pandoc 1.16
+            alt, [src, title] = value
+            attrs = None
+        else:
+            attrs, alt, [src, title] = value
+        mimet, _ = mimetypes.guess_type(src)
+        targetfmt = fmt_to_option.get(fmt)
+        if mimet == "image/svg+xml" and targetfmt:
+            base_name, _ = os.path.splitext(src)
+            eps_name = base_name + "." + targetfmt
+            cmd_line = ["make", eps_name]
+            sys.stderr.write("Running %s\n" % " ".join(cmd_line))
+            subprocess.call(cmd_line, stdout=sys.stderr.fileno())
+            if attrs:
+                return Image(attrs, alt, [eps_name, title])
+            else:
+                return Image(alt, [eps_name, title])
+
 
 if __name__ == "__main__":
-  toJSONFilter(svg_to_any)
+    toJSONFilter(svg_to_any)
