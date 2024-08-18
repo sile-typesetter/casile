e5dcb0ef07687dd06b4af68d495966a0d1bac6ae
Caleb Maclennan <caleb@alerque.com>
2024-08-18 23:35:52 +0300

    chore(import): Actually fix epigraph discovery with proper block iteration

--- pandoc-filters/mark-epigraphs.lua
+++ pandoc-filters/mark-epigraphs.lua
@@ -1,41 +1,21 @@
 local after_header
 
-local function header (element)
-   if element.level <= 2 then
-      after_header = true
+local function blocks (blocks)
+   for i, block in ipairs(blocks) do
+      if block.t == "Header" and block.level <= 2 then
+         after_header = true
+      elseif after_header then
+         if block.t == "BlockQuote" then
+            blocks[i] = pandoc.Div(block.content, { class = "epigraph" })
+         else
+            after_header = false
+         end
+      end
    end
-   return element
-end
-
--- Re-process block quotes following headers as epigraph class divs
-local function blockquote (element)
-   if after_header then
-      after_header = false
-      return pandoc.Div(element.content, { class = "epigraph" })
-   else
-      return element
-   end
-end
-
--- Elements that should not be expected between headers and epigraphs should reset the marker so following block quotes
--- no longer count as epigraphs
-local function other_block (element)
-   after_header = false
-   return element
+   return blocks
 end
 
 return {
-   { Header = header },
-   { BlockQuote = blockquote },
-   { BulletList = other_block },
-   { CodeBlock = other_block },
-   { DefinitionList = other_block },
-   { Div = other_block },
-   { Figure = other_block },
-   { HorizontalRule = other_block },
-   { LineBlock = other_block },
-   { Para = other_block },
-   { Plain = other_block },
-   { RawBlock = other_block },
-   { Table = other_block },
+   traverse = "topdown",
+   { Blocks = blocks }
 }
