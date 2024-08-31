a6ac22d6b737845a72f23095211537cc8a174496
Caleb Maclennan <caleb@alerque.com>
2024-09-01 00:52:05 +0300

    fix(layouts): Fixup halfletter frameset in new class design

--- layouts/halfletter.lua
+++ layouts/halfletter.lua
@@ -2,31 +2,25 @@ return function (class)
    class.options.papersize = "halfletter"
 
    if class._name == "cabook" then
-      class:loadPackage("masters", {
-         {
-            id = "right",
-            firstContentFrame = "content",
-            frames = {
-               content = {
-                  left = "left(page) + 22.5mm",
-                  right = "right(page) - 15mm",
-                  top = "top(page) + 20mm",
-                  bottom = "top(footnotes)",
-               },
-               runningHead = {
-                  left = "left(content)",
-                  right = "right(content)",
-                  top = "top(content) - 8mm",
-                  bottom = "top(content)",
-               },
-               footnotes = {
-                  left = "left(content)",
-                  right = "right(content)",
-                  height = "0",
-                  bottom = "bottom(page) - 15mm",
-               },
-            },
+      class.defaultFrameset = {
+         content = {
+            left = "left(page) + 22.5mm",
+            right = "right(page) - 15mm",
+            top = "top(page) + 20mm",
+            bottom = "top(footnotes)",
          },
-      })
+         runningHead = {
+            left = "left(content)",
+            right = "right(content)",
+            top = "top(content) - 8mm",
+            bottom = "top(content)",
+         },
+         footnotes = {
+            left = "left(content)",
+            right = "right(content)",
+            height = "0",
+            bottom = "bottom(page) - 15mm",
+         },
+      }
    end
 end
