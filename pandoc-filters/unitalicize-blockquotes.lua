55a86ee79c62827e6269c81269eb8099f849feec
Caleb Maclennan <caleb@alerque.com>
2024-11-07 11:24:51 +0300

    feat(import): Assume italicized blockquotes are redundant inline formatting

---
+++ pandoc-filters/unitalicize-blockquotes.lua
@@ -0,0 +1,17 @@
+local function unitalicize (element)
+   element = pandoc.walk_block(element, {
+      Para = function(el)
+         if #el.content == 1 then
+            local inner = el.content[1]
+            if inner.tag == "Emph" then
+               return pandoc.Plain(inner.content)
+            end
+         end
+      end
+   })
+   return element
+end
+
+return {
+   { BlockQuote = unitalicize },
+}
